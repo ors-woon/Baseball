751ab87363edbd1c281ad26182fe333c53b68617
Jangchulwoon <lusiue@naver.com>
2018-06-17 20:04:01 +0900

    add travis setting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,11 @@ public class Main {
         // test logic
 
         List list = Arrays.asList("hello","world");
-
+        String str = "";
         for(int i = 0; i<list.size(); i++){
-            System.out.println(list.get(i));
+            str += list.get(i);
         }
-        //
+        System.out.println(str.toString());
         // if sonarqube execute, It would announce waring message.
     }
 }
